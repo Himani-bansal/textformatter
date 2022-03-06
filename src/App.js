3af8d0b0da1865272a428177be3e00b3f69fb957
Himani bansal <91753535+Himani-bansal@users.noreply.github.com>
2022-03-06 15:13:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,12 +26,12 @@ function App() {
     if (mode==='light') {
        setMode('dark');
        document.body.style.backgroundColor='#13406d';  
-       showAlert("Dark mode has been enabled","success")    
+           
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white'; 
-      showAlert("Light mode has been enabled","success") 
+   
     }
   }
   
